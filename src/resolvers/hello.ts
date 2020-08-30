982f503c4fd84bf9759bb1a61d3d5b3733a54dac
keshavdulal <keshav.dulal@gmail.com>
2020-08-30 13:32:37 +0545

    MikroORM TypeGraphQL Crud

---
+++ src/resolvers/hello.ts
@@ -0,0 +1,10 @@
+//  hello schema using type-graphql
+import { Query, Resolver } from 'type-graphql';
+
+@Resolver()
+export class HelloResolver {
+  @Query(() => String)
+  hello() {
+    return 'hello world from type-graphql resolver';
+  }
+}
---
+++ src/resolvers/post.ts
@@ -0,0 +1,68 @@
+//  hello schema using type-graphql
+import { MyContext } from 'src/types';
+import { Arg, Ctx, Mutation, Query, Resolver } from 'type-graphql';
+import { Post } from '../entities/Posts';
+
+@Resolver()
+export class PostResolver {
+    // CREATE
+    @Mutation(() => Post)
+    // set type gql type inside @Arg (removed) followed by TS Type
+    async createPost(
+        @Arg('title') title: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post> {
+        const post = em.create(Post, { title });
+        await em.persistAndFlush(post);
+        return post;
+    }
+
+    // Read
+    @Query(() => Post, { nullable: true })
+    // set type gql type inside @Arg (removed) followed by TS Type
+    post(
+        @Arg('id') id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        return em.findOne(Post, { id });
+    }
+
+    // Read ALL
+    @Query(() => [Post])
+    posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+        return em.find(Post, {});
+    }
+
+    // UPDATE
+    @Mutation(() => Post, { nullable: true })
+    // set type gql type inside @Arg (removed) followed by TS Type
+    async updatePost(
+        @Arg('id') id: number,
+        @Arg('title', () => String, { nullable: true }) title: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post | null> {
+        const post = await em.findOne(Post, { id });
+        if (!post) {
+            return null;
+        }
+        if (typeof title !== undefined) {
+            post.title = title;
+            await em.persistAndFlush(post);
+        }
+        return post;
+    }
+
+    // DELETE
+    @Mutation(() => Boolean)
+    async deletePost(
+        @Arg('id') id: number,
+        @Ctx() { em }: MyContext
+    ): Promise<boolean> {
+        try {
+            await em.nativeDelete(Post, { id });
+        } catch {
+            return false;
+        }
+        return true;
+    }
+}
